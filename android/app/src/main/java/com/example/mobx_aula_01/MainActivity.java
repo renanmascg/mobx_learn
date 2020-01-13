f3440926678aea7792bbad44f0c6a25c8274c574
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-01-13 09:42:01 -0300

    Commit inicial para o mobx

---
+++ android/app/src/main/java/com/example/mobx_aula_01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobx_aula_01;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
