23609fd45fd522fb6f0492d9db6978417cc380e3
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-01-13 15:45:19 -0300

    mobx for news app

--- lib/home.dart
+++ lib/home.dart
@@ -1,49 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_aula_01/models/hacker_news.dart';
+import 'package:mobx_aula_01/serializers/news.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  HomeState createState() {
+    return new HomeState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeState extends State<Home> {
+  final _hacker_news = HackerNews();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final snackBar = SnackBar(
+    content: Text('News List will be update in sometime..'),
+    action: SnackBarAction(
+      label: 'Ok',
+      onPressed: () {
+        // Some code to undo the change!
+      },
+    ),
+  );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _hacker_news.getNewsList();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Hacker News'),
         ),
+        body: Observer(
+          builder: (context) => RefreshIndicator(
+            onRefresh: () async {
+              await Future.delayed(Duration(seconds: 1));
+              await _hacker_news.increaseNewsLimit();
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+            child: Container(
+              child: Observer(
+                  builder: (_) => ((_hacker_news.news != null) &&
+                          (_hacker_news.news.isNotEmpty))
+                      ? ListView.builder(
+                          itemCount: _hacker_news.news.length,
+                          itemBuilder: (_, index) {
+                            final newsAritcle = _hacker_news.news[index];
+                            return _makeArticleContainer(newsAritcle);
+                          },
+                        )
+                      : Center(
+                          child: CircularProgressIndicator(),
+                        )),
+            ),
+          ),
+        ),
+      );
+
+  Widget _makeArticleContainer(News newsArticle) {
+    return Padding(
+      key: Key(newsArticle.title),
+      padding: const EdgeInsets.all(16.0),
+      child: ExpansionTile(
+        title: Text(
+          newsArticle.title,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("${newsArticle.by} comments"),
+              IconButton(
+                onPressed: () async {
+                  if (await canLaunch(newsArticle.url)) {
+                    launch(newsArticle.url);
+                  }
+                },
+                icon: Icon(Icons.launch),
+              )
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
     );
   }
 }
